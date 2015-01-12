cd00ec18201b79368b6c749585fbe89de9544c8d
Hitman666 <nikola.breznjak@gmail.com>
2015-01-12 14:30:19 +0100

    finito

---
+++ Assets/Shooter.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+public var projectile : Rigidbody;
+public var shotPos : Transform;
+public var shotForce : float = 1000f;
+public var moveSpeed : float = 10f;
+
+
+function Update ()
+{
+    var h : float = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+    var v : float = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+    
+    transform.Translate(new Vector3(h, v, 0f));
+    
+    if(Input.GetButtonUp("Fire1"))
+    {
+        var shot : Rigidbody = Instantiate(projectile, shotPos.position, shotPos.rotation);
+        shot.AddForce(shotPos.forward * shotForce);
+    }
+}
